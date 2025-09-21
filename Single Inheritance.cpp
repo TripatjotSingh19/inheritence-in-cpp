227ce22a475f4c545b2a62b017ba2f67c1796427
TripatjotSingh19 <tripatjots646@gmail.com>
2025-09-21 23:07:24 +0530

    Create Single Inheritance.cpp

---
+++ Single Inheritance.cpp
@@ -0,0 +1,33 @@
+/* Tripatjot singh 
+PRN 24070123044
+ENTC a2 */
+
+#include<iostream>
+using namespace std;
+
+class Uni {
+public:
+    string uni = "Symbiosis";
+    void discipline() {
+        cout << "Engineering" << endl;
+    }
+};
+
+class Dep : public Uni {
+public:
+    string dept = "Electronics & Communication";
+};
+
+int main() {
+    Dep department;
+    cout << department.uni << endl;
+    department.discipline();
+    cout << department.dept << endl;
+
+    return 0;
+}
+
+// OUTPUT
+// Symbiosis
+// Engineering
+// Electronics & Communication
